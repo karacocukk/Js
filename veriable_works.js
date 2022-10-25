98eaedaa0ec5cf1654c8b41ff22b06403c8bcc4c
karacocukk <karacocukk>
2022-10-25 19:31:05 +0300

    ..

---
+++ veriable_works.js
@@ -0,0 +1,49 @@
+// let result = 3 + 5 
+// console.log(result);
+
+
+// let result = 8 - 5 
+// console.log(result);
+ 
+// let result = 3 * 5 
+// console.log(result);
+
+
+// let result = 3 ** 2 
+// console.log(result);
+
+
+// let result = 15 % 2 
+// console.log(result);
+
+// ********** Boolean Veri Tipi ile Çalışmak **********
+
+// 0 ve 1 ' i Anlamak
+// let isActive = false // 0
+// isActive = true  // 1
+// console.log(isActive)
+
+// let userName ; 
+// let isUserName = Boolean(userName)
+// console.log(isUserName)
+
+// Boolean("11") // True
+// Boolean("0")  // True
+// Boolean("")   // False
+// Boolean("-0") // False
+// Boolean("-0.1") // True
+
+// userName = "user"
+// console.log("User Name: ",Boolean(userName))
+
+
+// a = Boolean(userName.length > 0) // True 
+// console.log("Username > 0 ?:",a)
+
+
+
+console.log(Boolean(1n));
+console.log(Boolean(-1n));
+console.log(Boolean(Infinity));
+console.log(Boolean({}));
+console.log(Boolean(Symbol()));
